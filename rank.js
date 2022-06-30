c5b614357ef883f7dcbb21fc423fd1034a810898
Karpagam-Krishnasamy <karpagamkrishnasamy@gmail.com>
2022-06-30 15:07:48 +0530

    changes made on rank problem

--- rank.js
+++ rank.js
@@ -1,109 +1,116 @@
+const { values } = require("@laufire/utils/collection");
+
 const markSheets= [
   {
-      student: 'Sriram',
-      rollNo: 11,
-      tamil: 80,
-      english: 90,
-      science: 86,
-      maths: 97,
-      social: 76,
-  },
-  {
-      student: 'Ram',
-      rollNo: 16,
-      tamil: 90,
-      english: 97,
-      science: 100,
-      maths: 34,
-      social: 96,
-      
-  },
-  {
-      student: 'sri',
-      rollNo: 18,
-      tamil: 60,
-      english: 90,
-      science: 66,
-      maths: 93,
-      social: 46,
-  },
-  {
-      student: 'mani',
-      rollNo: 20,
-      tamil: 40,
-      english: 70,
-      science: 86,
-      maths: 73,
-      social: 86,
-  },
-  {
-      student: 'praveen',
-      rollNo: 80,
-      tamil: 90,
-      english: 80,
-      science: 86,
-      maths: 96,
-      social: 77,
-
-  },
-  {
-      student: 'thiru',
-      rollNo: 81,
-      tamil: 90,
-      english: 80,
-      science: 86,
-      maths: 96,
-      social: 77,
-
-  }
-  ,
-  {
-      student: 'manikandan',
-      rollNo: 82,
-      tamil: 100,
-      english: 100,
-      science: 34,
-      maths: 100,
-      social: 100,
-
-  }
+    student: 'Sriram',
+    rollNo: 11,
+},
+{
+    student: 'Ram',
+    rollNo: 16,    
+},
+{
+    student: 'sri',
+    rollNo: 18,
+},
+{
+    student: 'mani',
+    rollNo: 20,
+},
+{
+    student: 'praveen',
+    rollNo: 80,
+},
+{
+    student: 'thiru',
+    rollNo: 81,
+}
+,
+{
+    student: 'manikandan',
+    rollNo: 82,
+}
 ];
-let totalFail; 
-const getRank = (studentRecord) => {
-let rank=1;
-studentRecord.sort((firstRecord, secondRecord ) => secondRecord.total - firstRecord.total);
-const result = studentRecord.map(element => ({
-    ...element,rank : (element.passOrFail === 'P')?rank++:"-"
-  })
-    )
-totalFail = studentRecord.length - (--rank);
-   return result;
-};
+const marks={
+  11: {
+    tamil: 80,
+    english: 90,
+    science: 86,
+    maths: 97,
+    social: 76,
+  },
+  16: { 
+    tamil: 90,
+    english: 97,
+    science: 100,
+    maths: 34,
+    social: 96,
+  },
+  18: {
+    tamil: 60,
+    english: 90,
+    science: 63,
+    maths: 93,
+    social: 46,
+  },
+  20: {
+    tamil: 79,
+    english: 80,
+    science: 91,
+    maths: 93,
+    social: 86,
+  },
+  80: {
+    tamil: 90,
+    english: 80,
+    science: 86,
+    maths: 96,
+    social: 77,
+  },
+  81: {
+    tamil: 90,
+    english: 40,
+    science: 80,
+    maths: 68,
+    social: 77,
+  },
+  82: {
+    tamil: 100,
+    english: 100,
+    science: 34,
+    maths: 100,
+    social: 100,
+}
+}
 
-const getCounts = (studentRecord) => {
-  studentRecord["count"] ={};
-  studentRecord["count"]["pass"] = studentRecord.length - totalFail;
-  studentRecord["count"]["fail"] = totalFail;
-  return studentRecord;
-};
+let rank =1; 
 
-const passOrFail = (studentRecord) =>
-  studentRecord.map(element => ({  
-      ...element,passOrFail : (element.tamil <35 || element.english <35 || element.maths<35 || element.science <35 || element.social<35?"F":"P")
-})
-  );
+const getTotal = (studentDetail, marks) => 
+  values(marks[studentDetail.rollNo]).reduce((acc, c)=> acc + c);
 
-const processMarkSheet = (studentRecord) => 
-  studentRecord.map(element=>({
-      ...element,total : element.tamil + element.english + element.maths + element.science + element.social
-    })
-);
+const passOrFail = (studentDetail, marks) =>
+    Math.min(...values(marks[studentDetail.rollNo])) > 35 
+      ? 'P'
+      : 'F'
+
+const getRank = (studentRecord) =>
+studentRecord.map((data, index, array) =>
+  data.passOrFail === 'P'
+  ? (index === 0 || data.total === array[index-1]['total'] 
+    ? {...data, Rank: rank}
+    : ((rank = index+1), ({...data, Rank: rank})))
+  : {...data, Rank: '-'});
+
+const getCount = (studentRecord) => (
+  [...studentRecord, {count:{Pass: rank, Fail: studentRecord.length - rank}}]
+    );
 
 
-const processMarkSheets = (marSheets)=> {
-const recordWithTotal = processMarkSheet(marSheets);
-const result= passOrFail(recordWithTotal);
-return getCounts(getRank(result));
-};
+const processMarkSheets = (marSheets, marks)=>{
+  const studentRecord = marSheets.map(data =>({
+     ...data, total: getTotal(data,marks), passOrFail: passOrFail(data, marks)
+    })).sort((a, b)=> b.passOrFail.localeCompare(a.passOrFail) || b.total - a.total);
+    return getCount(getRank(studentRecord));
+  };
 
-console.log(processMarkSheets(markSheets));
+console.table(processMarkSheets(markSheets, marks));
