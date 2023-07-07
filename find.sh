00ae87f8486b7f6b55f43edf35aa2ff94553c511
Karpagam-Krishnasamy <karpagamkrishnasamy@gmail.com>
2023-07-07 12:55:59 +0530

    Introduced functions in script.

--- find.sh
+++ find.sh
@@ -1,1 +1,10 @@
-find -maxdepth 1 -type d -exec test ! -d "{}/.git" \; -print
+filterNonGitFolders()
+{
+  for topdir in ./*/;
+do
+    [ ! "$(find "$topdir" -name .git)" ] &&
+    echo "$topdir"
+done
+}
+
+filterNonGitFolders
